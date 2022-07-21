3fb8e400c5c831ad931519798d4460ef714ade1b
bagashiz <bagashiz@proton.me>
2022-07-21 22:39:56 +0700

    built complex template cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,62 +1,87 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
-// tc is a map that stores the templates that have been loaded
-var tc = make(map[string]*template.Template)
+// RenderTemplate is a function that renders a template
+func RenderTemplate(w http.ResponseWriter, html string) {
+	// create template cache
+	templateCache, err := CreateTemplateCache()
+	// check for any errors
+	if err != nil {
+		// exit the application
+		log.Fatal(err)
+	}
 
-// RenderTemplate is a function that renders the specified template to the response writer
-func RenderTemplate(w http.ResponseWriter, h string) {
-	// create variables to store the template and any errors
-	var html *template.Template
-	var err error
+	// get the requested template from the cache
+	h, ok := templateCache[html]
+	// check if the template is in the cache
+	if !ok {
+		// exit the application
+		log.Fatal(err)
+	}
 
-	// check if the template has already been loaded into the map
-	_, inMap := tc[h]
-	if !inMap {
-		// create the template
-		log.Printf("Creating template cache and adding %s to the cache\n", h)
-		err = createTemplateCache(h)
-		// check for any errors
-		if err != nil {
-			log.Printf("Error creating template cache: %v\n", err)
+	// create buffer to hold the rendered html
+	buf := new(bytes.Buffer)
 
-		} else {
-			// get the template from the map
-			log.Printf("Using cached template for %v\n", h)
-		}
+	// execute the template
+	h.Execute(buf, nil)
 
-		// assign the template to the html variable and check for any errors
-		html = tc[h]
-		err = html.Execute(w, nil)
-		// check for any errors
-		if err != nil {
-			log.Printf("Error executing template: %v\n", err)
-		}
+	// render the template
+	_, err = buf.WriteTo(w)
+	// check for any errors
+	if err != nil {
+		log.Printf("Error writing template %v: %v\n", html, err)
 	}
 }
 
-func createTemplateCache(h string) error {
-	// create the templates slice to store the templates that are loaded
-	templates := []string{
-		fmt.Sprintf("./templates/%s", h),
-		"./templates/base.html",
-	}
+// CreateTemplateCache is a function that creates a template cache to store the templates in memory for faster rendering
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	// create a map to hold the templates
+	myCache := map[string]*template.Template{}
 
-	// parse the templates
-	html, err := template.ParseFiles(templates...)
+	// get all of the html files in the templates directory
+	pages, err := filepath.Glob("./templates/*.html")
 	// check for any errors
 	if err != nil {
-		return err
+		return myCache, err
 	}
 
-	// add the template to the map
-	tc[h] = html
+	// loop through the html files
+	for _, page := range pages {
+		// get the file name
+		pageName := filepath.Base(page)
 
-	return nil
+		// parse the html file into a template object
+		templateSet, err := template.New(pageName).ParseFiles(page)
+		// check for any errors
+		if err != nil {
+			return myCache, err
+		}
+
+		// check if the template is already in the cache
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		// check for any errors
+		if err != nil {
+			return myCache, err
+		}
+
+		// check if there are any layout templates
+		if len(matches) > 0 {
+			// parse the layout template into a template object
+			templateSet, err = templateSet.ParseGlob("./templates/*.layout.html")
+			// check for any errors
+			if err != nil {
+				return myCache, err
+			}
+
+			myCache[pageName] = templateSet
+		}
+	}
+	return myCache, nil
 }

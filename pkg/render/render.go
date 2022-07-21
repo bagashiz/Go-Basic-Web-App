92bfef00d4b3ad8646d9d30a2aefae47bc9e73e8
bagashiz <bagashiz@proton.me>
2022-07-21 21:15:19 +0700

    refactoring templates code

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -9,7 +9,7 @@ import (
 // RenderTemplate is a function that renders the specified template to the response writer
 func RenderTemplate(w http.ResponseWriter, html string) {
 	// Load the template from the templates folder and parse it into a html template object
-	parsedTemplate, _ := template.ParseFiles("./templates/" + html)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+html, "./templates/base.html")
 	err := parsedTemplate.Execute(w, nil)
 	// check for any errors
 	if err != nil {
